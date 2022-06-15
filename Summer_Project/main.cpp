9ddada4bf50f6e73aee2609db8a9454bf6e37667
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2022-06-15 13:42:36 +0900

    Updata DxLib Spec

--- Summer_Project/main.cpp
+++ Summer_Project/main.cpp
@@ -6,16 +6,31 @@ int backgroundimage,pauseimage;
 int catimage, appleimages;
 int key = 0;
 
-int main(void) {
+int Oldkey, Nowkey, KeyFlg;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdshow)
+{
+	SetMainWindowText("");
+	ChangeWindowMode(TRUE);
+
+	if (DxLib_Init() == -1) return -1;
+	if (menu.LoadImages() == -1) return -1;
+	SetDrawScreen(DX_SCREEN_BACK);
 
 	enum GAME_MODE { STARRT = 0, MENU, OPTION, END };
 
 	std::cout << "Beta!" << '\n';
 
-	while (key != END)
+	while (ProcessMessage() == 0 && key != END && !(KeyFlg & PAD_INPUT_START))
 	{
 		std::cout << "MENU =1\nOPTION = 2\nEND = 3" << '\n' << std::flush;
 		std::cin >> key;
+
+		Oldkey = Nowkey;
+		Nowkey = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+		KeyFlg = Nowkey & ~Oldkey;
+
+		ClearDrawScreen();
 
 		switch (key)
 		{
@@ -24,7 +39,8 @@ int main(void) {
 			break;
 		case END:
 			std::cout << "Exit";
-			std::exit(0);
+			//std::exit(0);
+			DxLib_End();
 			break;
 
 
@@ -34,12 +50,14 @@ int main(void) {
 			break;
 		}
 	}
+	DxLib_End();
 
 	return 0;
 }
 
 void Menu::menu(void) {
 
+	DrawGraph(0, 0, backgroundimage, FALSE);
 	std::cout << "Menu" << std::endl;
 }	
 
@@ -63,4 +81,6 @@ void Menu::GameModeChange(int num) {
 int Menu::LoadImages(void) {
 	if ((backgroundimage = LoadGraph("images/background.jpg")) == -1) return -1;
 	if ((pauseimage = LoadGraph("images/pause.jpg")) == -1) return -1;
+
+	return 0;
 }
